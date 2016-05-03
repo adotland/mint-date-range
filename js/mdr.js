d9ec331b40e5d922cb7e5bf4244a0f94915b3570
adotland <a@dotlandconsulting.com>
2016-05-02 17:24:04 -0700

    1.0 release
    Don't bother using querystring because mint will only use it the first
    time page is loaded. All subsequent attempts to change date will be made
    using location object. Changing the querystring will actually cause a
    synchronization issue that will break the date filtering.

--- js/mdr.js
+++ js/mdr.js
@@ -11,41 +11,41 @@ $(document).ready(function () {
             PAGE_ERROR:         "#MDR_PageErrorMsg",
             INPUT_ERROR:        "#MDR_InputErrorMsg"
         },
-        
+
         invalidDateValue: "invalidDate"
     };
 
     var MDR = {
-    
+
         tabURL: "",
-		
+
 		valiDATE: function (date) {
 			var dateStr = MDR_constants.invalidDateValue;
-            
+
             if (date !== "") {
             //TODO: set object somewhere here for comparison on submit
                 var dateObj = new Date(date),
                     month = dateObj.getMonth(),
                     day = dateObj.getDate(),
                     year = dateObj.getFullYear();
-                    
+
                 if (!isNaN(year) && year < 9999) {
-                    
+
                     dateStr = (month + 1) + "/" + day + "/" + year;
-                    
+
                 }
-                
+
             } else {
-            
+
                 return "";
-            
+
             }
-            
+
 			return dateStr;
 		},
-        
+
         runValidation: function () {
-        
+
             $(MDR_constants.selectors.MDR_TO + "," + MDR_constants.selectors.MDR_FROM).each(function () {
                 var date = $(this).val(),
                     validatedDate = MDR.valiDATE(date);
@@ -64,213 +64,184 @@ $(document).ready(function () {
                 }
             });
         },
-        
+
         handleSubmit: function(event) {
-                    
+
             if ($(MDR_constants.selectors.MDR_SUBMIT).hasClass("ready")) {
 
                 return MDR.doSubmit();
 
             } else {
-                
+
                 event.preventDefault();
                 return false;
 
             }
-            
+
         },
-        
+
         doSubmit: function () {
-            
+
             chrome.tabs.getSelected(null, function (tab) {
-            
+
                 $(MDR_constants.selectors.ERROR_MSGS).hide();
                 MDR.tabURL = tab.url;
-					
+
                 if (MDR.tabURL.match(/https\:\/\/wwws\.mint\.com\/(\w+)\.event/) !== null) {
-            
+
                     var fromDate    = MDR.valiDATE($(MDR_constants.selectors.MDR_FROM).val()),
                         toDate      = MDR.valiDATE($(MDR_constants.selectors.MDR_TO).val()),
                         fromDateObj = $(MDR_constants.selectors.MDR_FROM).data("theDate"),
-                        toDateObj   = $(MDR_constants.selectors.MDR_TO).data("theDate"),
-                        setTo       = false;
-                                                
+                        toDateObj   = $(MDR_constants.selectors.MDR_TO).data("theDate");
+
                     if (fromDateObj > toDateObj) {
-                    
+
                         $(MDR_constants.selectors.DATE_RANGE_ERROR).show();
                         return false;
-                        
+
                     }
-                    
+
                     if ((toDate !== MDR_constants.invalidDateValue) || (fromDate !== MDR_constants.invalidDateValue)) {
-                    
-                        if (MDR.tabURL.indexOf("endDate") > -1) {
-                        
-                            MDR.tabURL = MDR.tabURL.replace(/endDate=\d{1,2}\/\d{1,2}\/\d{4}\&?/, "");
 
-                        }
-                        
-                        if (MDR.tabURL.indexOf("startDate") > -1) {
-                        
-                            MDR.tabURL = MDR.tabURL.replace(/startDate=\d{1,2}\/\d{1,2}\/\d{4}\&?/, "");
+                        MDR.updateTabUrl(false, toDate);
+                        MDR.updateTabUrl(true, fromDate);
 
-                        }
-                                       
-                        var dateArray = [],
-                            locStart = null,
-                            qStart = null,
-                            pStart = null;
-                       
-                        if (toDate !== "") {
-
-                            dateArray = [];
-                            qStart = MDR.tabURL.indexOf("?");
-                            pStart = MDR.tabURL.indexOf("#");
-                                                    
-                            if (qStart > -1) {
-                            
-                                MDR.tabURL = MDR.tabURL.slice(0, qStart + 1) + "endDate=" + toDate + "&" + MDR.tabURL.slice(qStart + 1);
-                            
-                            } else if (pStart > -1) {
-                            
-                                MDR.tabURL = MDR.tabURL.slice(0, pStart) + "?endDate=" + toDate + "&" + MDR.tabURL.slice(pStart);
-                            
-                            } else {
-                            
-                                MDR.tabURL += "?endDate=" + toDate;
-                                
-                            }
-
-                            if (MDR.tabURL.indexOf("dateEnd") > -1) {
-                            
-                                dateArray = toDate.split("/");
-                                MDR.tabURL = MDR.tabURL.replace(/%22dateEnd%22%3A%22\d{1,2}%2F\d{1,2}%2F\d{4}/, "%22dateEnd%22%3A%22" + dateArray[0] + "%2F" + dateArray[1] + "%2F" + dateArray[2]);
-
-                            } else if (MDR.tabURL.indexOf("#location:%7B") > -1) {
-                                 
-                                dateArray = toDate.split("/");
-                                locStart = MDR.tabURL.indexOf("#location:%7B");
-                                MDR.tabURL = MDR.tabURL.slice(0, locStart + 13) + "%22dateEnd%22%3A%22" + dateArray[0] + "%2F" + dateArray[1] + "%2F" + dateArray[2] + "%22%2C" + MDR.tabURL.slice(locStart + 13);
-                            
-                            }
-                            
-                            setTo = true;
-
-                        }
-                        
-                        if (fromDate !== "") {
-                        
-                            if (MDR.tabURL === "") {
-                                
-                                MDR.tabURL = tab.url;
-                            
-                            }
-                            
-                            dateArray = [];
-                            qStart = MDR.tabURL.indexOf("?");
-                            pStart = MDR.tabURL.indexOf("#");
-                            
-                            if (qStart > -1) {
-                            
-                                MDR.tabURL = MDR.tabURL.slice(0, qStart + 1) + "startDate=" + fromDate + "&" + MDR.tabURL.slice(qStart + 1);
-                            
-                            } else if (pStart > -1) {
-                            
-                                MDR.tabURL = MDR.tabURL.slice(0, pStart) + "?startDate=" + fromDate + "&" + MDR.tabURL.slice(pStart);
-                            
-                            } else {
-                            
-                                MDR.tabURL += "?startDate=" + fromDate;
-                            
-                            }
-                            
-                            if (MDR.tabURL.indexOf("dateStart") > -1) {
-                            
-                                dateArray = fromDate.split("/");
-                                MDR.tabURL = MDR.tabURL.replace(/%22dateStart%22%3A%22\d{1,2}%2F\d{1,2}%2F\d{4}/, "%22dateStart%22%3A%22" + dateArray[0] + "%2F" + dateArray[1] + "%2F" + dateArray[2]);
-
-                            } else if (MDR.tabURL.indexOf("#location:%7B") > -1) {
-                                 
-                                dateArray = fromDate.split("/");
-                                locStart = MDR.tabURL.indexOf("#location:%7B");
-                                MDR.tabURL = MDR.tabURL.slice(0, locStart + 13) + "%22dateStart%22%3A%22" + dateArray[0] + "%2F" + dateArray[1] + "%2F" + dateArray[2] + "%22%2C" + MDR.tabURL.slice(locStart + 13);
-                            
-                            }
-                            
-                        }
-                            
                         if (MDR.tabURL !== "") {
-                        
+
                             if (MDR.tabURL.match(/https\:\/\/wwws\.mint\.com\/(\w+)\.event/)[1] === "transaction") {
-                            
+
                                 chrome.tabs.update(tab.id, {url: MDR.tabURL});
                                 window.close();
-                                
+
                             } else {
-                            
+
                                 $(MDR_constants.selectors.PAGE_ERROR).show();
-                            
+
                             }
-                       
+
                         }
-                    
+
                     } else {
-                    
+
                         $(MDR_constants.selectors.INPUT_ERROR).show();
-                        
+
                     }
-               
+
                 } else {
-                        
+
                     $(MDR_constants.selectors.PAGE_ERROR).show();
-                
+
                 }
-               
+
             });
 
+        },
+
+        updateTabUrl: function (startNotEnd, dateStr) {
+
+            var dateArray = [],
+                locStr = null,
+                locStartStr = "#location:%7B",
+                locStart = MDR.tabURL.indexOf(locStartStr),
+                locDateStr = null,
+                locDateToken = startNotEnd ? "dateStart" : "dateEnd";
+
+            if (MDR.tabURL === "") {
+
+                MDR.tabURL = tab.url;
+
+            }
+
+            if (dateStr !== "") {
+
+                MDR.tabURL = MDR.tabURL.replace(/%22period%22%3A%22ALL%22%2C/, "");
+
+                dateArray = dateStr.split("/");
+                locDateStr = "%22" + locDateToken + "%22%3A%22" + dateArray[0] + "%2F" + dateArray[1] + "%2F" + dateArray[2] + "%22";
+
+                if (MDR.tabURL.indexOf(locDateToken) > -1) {
+
+                    if (startNotEnd) {
+
+                        MDR.tabURL = MDR.tabURL.replace(/%22dateStart%22%3A%22\d{1,2}%2F\d{1,2}%2F\d{4}%22/, locDateStr);
+
+                    } else {
+
+                        MDR.tabURL = MDR.tabURL.replace(/%22dateEnd%22%3A%22\d{1,2}%2F\d{1,2}%2F\d{4}%22/, locDateStr);
+
+                    }
+
+                } else if (locStart > -1) {
+
+                    MDR.tabURL = MDR.tabURL.slice(0, locStart + 13) + locDateStr + "%2C" + MDR.tabURL.slice(locStart + 13);
+
+                } else {
+
+                    MDR.tabURL += locStartStr + locDateStr + "%2C%22query%22%3A%22%22%2C%22offset%22%3A0%2C%22typeFilter%22%3A%22cash%22%7D";
+
+                }
+
+            } else {
+                
+                if (MDR.tabURL.indexOf(locDateToken) > -1) {
+
+                    if (startNotEnd) {
+
+                        MDR.tabURL = MDR.tabURL.replace(/%22dateStart%22%3A%22\d{1,2}%2F\d{1,2}%2F\d{4}%22%2C/, "");
+
+                    } else {
+
+                        MDR.tabURL = MDR.tabURL.replace(/%22dateEnd%22%3A%22\d{1,2}%2F\d{1,2}%2F\d{4}%22%2C/, "");
+
+                    }
+
+                }
+
+            }
+
         }
-    
+
     };
 
     // Event binding
     $(MDR_constants.selectors.MDR_FROM).glDatePicker({
-                        
+
         onChange : function ($target, newDate) {
-                        
+
             $(MDR_constants.selectors.MDR_SUBMIT).show();
             $(MDR_constants.selectors.MDR_TO).data("theDate", newDate);
             $(MDR_constants.selectors.MDR_TO).data("settings").startDate = newDate;
             MDR.runValidation($target);
         }
-        
+
     });
 
     $(MDR_constants.selectors.MDR_TO).glDatePicker({
-                    
+
         allowOld    : false,
         onChange    : function ($target, newDate) {
-            
+
             $(MDR_constants.selectors.MDR_SUBMIT).show();
             MDR.runValidation($target);
-            
+
         }
-        
+
     });
 
     $(MDR_constants.selectors.MDR_SUBMIT).click(function (event) {
-        
+
         MDR.handleSubmit(event);
-        
+
     });
 
     $(MDR_constants.selectors.MDR_FROM + "," + MDR_constants.selectors.MDR_TO)
         .bind("blur", function (event) {
-        
+
             var $target = $(event.target);
             MDR.runValidation($target);
-        
+
         })
-        
+
         .bind("keyup", function (event) {
 
             if (event.keyCode === 13) {
@@ -283,9 +254,9 @@ $(document).ready(function () {
         })
 
         .bind("focus", function (event) {
-        
+
             $(MDR_constants.selectors.INPUT_ERROR).hide();
-        
+
         });
 
 	// Init
