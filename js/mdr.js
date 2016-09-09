9e3ee9d5ff18dd58f478822c62389ed6935d9ce0
adotland <a@dotlandconsulting.com>
2016-09-08 22:12:23 -0700

    Store previous date input
    Automatically store previous dates searched, allow user to clear dates.

--- js/mdr.js
+++ js/mdr.js
@@ -9,10 +9,33 @@ $(document).ready(function () {
             MDR_SUBMIT:         "#MDR_Submit",
             DATE_RANGE_ERROR:   "#MDR_DateRangeErrorMsg",
             PAGE_ERROR:         "#MDR_PageErrorMsg",
-            INPUT_ERROR:        "#MDR_InputErrorMsg"
+            INPUT_ERROR:        "#MDR_InputErrorMsg",
+            RESET_DATE:         ".mdr-reset-button",
+            INPUT_CONTAINER:    ".mdr-date-box-container"
         },
 
-        invalidDateValue: "invalidDate"
+        selectorNames: {
+            SUBMIT_READY:       "mdr-ready",
+            INVALID_INPUT:      "mdr-invalid-input",
+            DATE_BOX_FOCUS:     "mdr-date-box-focus",
+            DATE_BOX:           "mdr-date-box"
+        },
+
+        INVALID_DATE_VALUE: "invalidDate",
+        MINT_URLS: [/https\:\/\/wwws\.mint\.com\/(\w+)\.event/, /https\:\/\/mint\.intuit\.com\/(\w+)\.event/]
+    };
+
+    var MDR_cached = {
+
+        $FromAndToInputs:   $(MDR_constants.selectors.MDR_FROM + "," + MDR_constants.selectors.MDR_TO),
+        $SubmitButton:      $(MDR_constants.selectors.MDR_SUBMIT),
+        $FromInput:         $(MDR_constants.selectors.MDR_FROM),
+        $ToInput:           $(MDR_constants.selectors.MDR_TO),
+        $ErrorMsgs:         $(MDR_constants.selectors.ERROR_MSGS),
+        $InputError:        $(MDR_constants.selectors.INPUT_ERROR),
+        $ResetDate:         $(MDR_constants.selectors.RESET_DATE),
+        $InputContainers:   $(MDR_constants.selectors.INPUT_CONTAINER)
+
     };
 
     var MDR = {
@@ -20,7 +43,7 @@ $(document).ready(function () {
         tabURL: "",
 
 		valiDATE: function (date) {
-			var dateStr = MDR_constants.invalidDateValue;
+			var dateStr = MDR_constants.INVALID_DATE_VALUE;
 
             if (date !== "") {
             //TODO: set object somewhere here for comparison on submit
@@ -44,30 +67,56 @@ $(document).ready(function () {
 			return dateStr;
 		},
 
-        runValidation: function () {
+        validateAndStore: function ($inputField, fromNotTo) {
 
-            $(MDR_constants.selectors.MDR_TO + "," + MDR_constants.selectors.MDR_FROM).each(function () {
-                var date = $(this).val(),
-                    validatedDate = MDR.valiDATE(date);
+            var date = $inputField.val(),
+                validatedDate = MDR.valiDATE(date),
+                storageKeyObj = {},
+                storageKeyName = fromNotTo ? "fromDate" : "toDate";
 
-                if (validatedDate === MDR_constants.invalidDateValue || (validatedDate === "" && $(this).attr("id") !== "MDR_To")) {
+            storageKeyObj[storageKeyName] = validatedDate;
 
-                    $(MDR_constants.selectors.MDR_SUBMIT).removeClass("ready");
-                    $(MDR_constants.selectors.INPUT_ERROR).show();
-                    return false;
+            if ((validatedDate === MDR_constants.INVALID_DATE_VALUE) || (validatedDate === "" && fromNotTo)) {
 
-                } else {
+                MDR.resetDate(fromNotTo);
+                $inputField.addClass(MDR_constants.selectorNames.INVALID_INPUT);
+                MDR.updateSubmitDisplayStatus(false); // TODO: rewrite with MVC...
 
-                    $(MDR_constants.selectors.MDR_SUBMIT).addClass("ready");
-                    $(MDR_constants.selectors.INPUT_ERROR).hide();
+            } else {
 
+                $inputField.removeClass(MDR_constants.selectorNames.INVALID_INPUT);
+                if (fromNotTo || validatedDate !== "" && ! fromNotTo) {
+                    $inputField.next().next().show(); // show reset button
                 }
-            });
+                chrome.storage.local.set(storageKeyObj, function() {});
+                MDR.updateSubmitDisplayStatus(true);
+
+            }
+        },
+
+        resetDate: function (fromNotTo) {
+            var storageKeyName = fromNotTo ? "fromDate" : "toDate";
+            chrome.storage.local.remove(storageKeyName, function() {});
+        },
+
+        updateSubmitDisplayStatus: function (inputValid) {
+            if (inputValid) {
+                MDR_cached.$InputError.hide();
+                if (! MDR_cached.$FromInput.hasClass(MDR_constants.selectorNames.INVALID_INPUT) &&
+                    ! MDR_cached.$ToInput.hasClass(MDR_constants.selectorNames.INVALID_INPUT))
+                    MDR_cached.$SubmitButton.addClass(MDR_constants.selectorNames.SUBMIT_READY)
+            } else {
+                MDR_cached.$InputError.show();
+                MDR_cached.$SubmitButton.removeClass(MDR_constants.selectorNames.SUBMIT_READY);  
+            }
         },
 
         handleSubmit: function (event) {
 
-            if ($(MDR_constants.selectors.MDR_SUBMIT).hasClass("ready")) {
+            MDR.validateAndStore(MDR_cached.$FromInput, true);
+            MDR.validateAndStore(MDR_cached.$ToInput, false);
+
+            if (MDR_cached.$SubmitButton.hasClass(MDR_constants.selectorNames.SUBMIT_READY)) {
 
                 return MDR.doSubmit();
 
@@ -84,10 +133,10 @@ $(document).ready(function () {
 
             chrome.tabs.getSelected(null, function (tab) {
 
-                $(MDR_constants.selectors.ERROR_MSGS).hide();
+                MDR_cached.$ErrorMsgs.hide();
                 MDR.tabURL = tab.url;
 
-                var mintUrls = [/https\:\/\/wwws\.mint\.com\/(\w+)\.event/, /https\:\/\/mint\.intuit\.com\/(\w+)\.event/],
+                var mintUrls = MDR_constants.MINT_URLS,
                     validUrl,
                     matchUrlArr,
                     transactionPageMatch;
@@ -104,10 +153,10 @@ $(document).ready(function () {
 
                 if (validUrl) {
 
-                    var fromDate    = MDR.valiDATE($(MDR_constants.selectors.MDR_FROM).val()),
-                        toDate      = MDR.valiDATE($(MDR_constants.selectors.MDR_TO).val()),
-                        fromDateObj = $(MDR_constants.selectors.MDR_FROM).data("theDate"),
-                        toDateObj   = $(MDR_constants.selectors.MDR_TO).data("theDate");
+                    var fromDate    = MDR.valiDATE(MDR_cached.$FromInput.val()),
+                        toDate      = MDR.valiDATE(MDR_cached.$ToInput.val()),
+                        fromDateObj = MDR_cached.$FromInput.data("theDate"),
+                        toDateObj   = MDR_cached.$ToInput.data("theDate");
 
                     if (fromDateObj > toDateObj) {
 
@@ -116,7 +165,7 @@ $(document).ready(function () {
 
                     }
 
-                    if ((toDate !== MDR_constants.invalidDateValue) || (fromDate !== MDR_constants.invalidDateValue)) {
+                    if ((toDate !== MDR_constants.INVALID_DATE_VALUE) || (fromDate !== MDR_constants.INVALID_DATE_VALUE)) {
 
                         MDR.updateTabUrl(false, toDate);
                         MDR.updateTabUrl(true, fromDate);
@@ -138,7 +187,7 @@ $(document).ready(function () {
 
                     } else {
 
-                        $(MDR_constants.selectors.INPUT_ERROR).show();
+                        MDR_cached.$InputError.show();
 
                     }
 
@@ -282,42 +331,93 @@ $(document).ready(function () {
     };
 
     // Event binding
-    $(MDR_constants.selectors.MDR_FROM).glDatePicker({
 
-        onChange : function ($target, newDate) {
+    fromDatePicker = {};
+    toDatePicker = {};
+    
+    chrome.storage.local.get("fromDate", function(result) {
+        var startDate = new Date(),
+            selectedDate = -1; // gldp default value
 
-            $(MDR_constants.selectors.MDR_SUBMIT).show();
-            $(MDR_constants.selectors.MDR_TO).data("theDate", newDate);
-            $(MDR_constants.selectors.MDR_TO).data("settings").startDate = newDate;
-            MDR.runValidation($target);
+        if (result.hasOwnProperty("fromDate") && result.fromDate !== "") {
+            selectedDate = result.fromDate;
+            startDate = selectedDate;
+            MDR_cached.$FromInput.val(selectedDate);
+            MDR_cached.$FromInput.data("theDate", new Date(selectedDate));
+            MDR_cached.$FromInput.next().show() // show reset button            
         }
 
+        fromDatePicker = MDR_cached.$FromInput.glDatePicker({
+
+            selectedDate    : new Date(selectedDate),
+            startDate       : new Date(startDate),
+            
+            onChange        : function ($target, newDate) {
+
+                var $fieldResetButton = $target.siblings(MDR_cached.$ResetDate);
+
+                if ($target.val() !== "") {
+                    $fieldResetButton.show();
+                } else {
+                    $fieldResetButton.hide();
+                }
+
+                MDR_cached.$SubmitButton.show();
+                MDR.validateAndStore($target, true);
+            }
+
+        });
     });
 
-    $(MDR_constants.selectors.MDR_TO).glDatePicker({
+    chrome.storage.local.get("toDate", function(result) {
+        var startDate = new Date(),
+            selectedDate = -1; // gldp default value
 
-        allowOld    : false,
-        onChange    : function ($target, newDate) {
-
-            $(MDR_constants.selectors.MDR_SUBMIT).show();
-            MDR.runValidation($target);
-
+        if (result.hasOwnProperty("toDate") && result.toDate !== "") {
+            selectedDate = result.toDate;
+            startDate = selectedDate;
+            MDR_cached.$ToInput.val(selectedDate);
+            MDR_cached.$ToInput.data("theDate", new Date(selectedDate));
+            MDR_cached.$ToInput.next().show() // show reset button
         }
 
+        toDatePicker = MDR_cached.$ToInput.glDatePicker({
+
+            selectedDate    : new Date(selectedDate),
+            startDate       : new Date(startDate),
+            onChange        : function ($target, newDate) {
+
+                var $fieldResetButton = $target.siblings(MDR_cached.$ResetDate);
+
+                if ($target.val() !== "") {
+                    $fieldResetButton.show();
+                } else {
+                    $fieldResetButton.hide();
+                }
+
+                MDR_cached.$SubmitButton.show();
+                MDR.validateAndStore($target, false);
+
+            }
+
+        });
     });
 
-    $(MDR_constants.selectors.MDR_SUBMIT).click(function (event) {
+    MDR_cached.$SubmitButton.click(function (event) {
 
         MDR.handleSubmit(event);
 
     });
 
-    $(MDR_constants.selectors.MDR_FROM + "," + MDR_constants.selectors.MDR_TO)
-        .bind("blur", function (event) {
+    MDR_cached.$FromAndToInputs
 
-            var $target = $(event.target);
-            MDR.runValidation($target);
+        .change( function (event) {
 
+            var $target = $(event.target)
+            if (! $target.hasClass(MDR_constants.selectorNames.GLDP)) {
+                MDR.validateAndStore($target, true);
+                MDR.validateAndStore($target, false);
+            }
         })
 
         .bind("keyup", function (event) {
@@ -333,12 +433,37 @@ $(document).ready(function () {
 
         .bind("focus", function (event) {
 
-            $(MDR_constants.selectors.INPUT_ERROR).hide();
+            MDR_cached.$InputError.hide();
 
         });
 
+    MDR_cached.$ResetDate.click(function (event) {
+
+        var $resetButton = $(event.target),
+            input = $resetButton.parent().children()[0],
+            today = new Date();
+
+        input.value = "";
+        $resetButton.hide();
+
+        if (input.id === "MDR_From") {
+            MDR_cached.$SubmitButton.removeClass(MDR_constants.selectorNames.SUBMIT_READY);
+            MDR.resetDate(true);
+            fromDatePicker.glDatePicker("setStartDate", today);
+            fromDatePicker.glDatePicker("setSelectedDate", -1);
+            MDR_cached.$FromInput.data("theDate", new Date());
+            fromDatePicker.glDatePicker("update");
+        } else {
+            MDR.resetDate(false);
+            toDatePicker.glDatePicker("setStartDate", today);
+            toDatePicker.glDatePicker("setSelectedDate", -1);
+            MDR_cached.$ToInput.data("theDate", new Date());
+            toDatePicker.glDatePicker("update");
+        }
+    });
+
 	// Init
-    $(MDR_constants.selectors.MDR_FROM).focus();
+    MDR_cached.$FromInput.focus();
 
     // GA
     var _gaq = _gaq || [];
