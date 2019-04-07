d6de029041e7069e5027a68776332a7bfbe7aea2
adotland <adotland@gmail.com>
2019-04-07 12:02:57 -0700

    update version

--- js/bg.js
+++ js/bg.js
@@ -1,23 +1,23 @@
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                conditions: [
-                    new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: { urlPrefix: "https://wwws.mint.com/" }
-                    }),
-                    new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: { urlPrefix: "https://mint.intuit.com/" }
-                    }),
-                    new chrome.declarativeContent.PageStateMatcher({
-                        css: ['a[href$="transaction.event"].selected']
-                    }),
-                    new chrome.declarativeContent.PageStateMatcher({
-                        css: [".TransactionPageView"]
-                    })
-                ],
-                actions: [ new chrome.declarativeContent.ShowPageAction() ]
-            }
-        ]);
-    });
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlPrefix: "https://wwws.mint.com/" }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlPrefix: "https://mint.intuit.com/" }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            css: ['a[href$="transaction.event"].selected']
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            css: [".TransactionPageView"]
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
 });
